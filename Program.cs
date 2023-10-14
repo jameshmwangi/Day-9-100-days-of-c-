3ce213fdcfda3e87d505511232c349acbfe396e1
James Kamau <jameshmwangiy@gmail.com>
2023-10-14 08:20:43 +0300

    second challange on data type conversion

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@
 // else
 
 //     {Console.WriteLine("Unable to report the measuremennt");}
-stringring[] values={"12.3","45","ABC","11","DEF"};
-
+string[] values={"12.3","45","ABC","11","DEF"};
+decimal total= 0m;
+string message= "";
 foreach(var value in values)
 {
     decimal number;
@@ -23,3 +24,19 @@ foreach(var value in values)
 }
 Console.WriteLine($"Message:{message}");
 Console.WriteLine($"Total:{total}");
+
+// challange number two
+/* 
+Complete a challenge to output math 
+operations as specific number types
+*/
+int value1 =12;
+decimal value2=6.2m;
+float value3=4.3f;
+
+int result1= Convert.ToInt32((decimal)value1/value2 );
+decimal result2= value2/(decimal)value3;
+float result3= (value3/value1);
+Console.WriteLine($"Divide value1 by value2, display the result as an integer:{result1}");
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal:{result2}");
+// Console.WriteLine($"Divide value3 by value1, display the result as a float:{result3}");
